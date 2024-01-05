92f36b8ec0e70affcf67a6047cae86f796f6eb91
EGhost <Adityachauhan8901@gmail.com>
2024-01-05 15:58:13 +0530

    Swagger Added and AUTH Swagger Schema Updated

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LogoutView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, LogoutView, CustomTokenObtainPairView, CustomTokenRefreshView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', RegisterView.as_view(), name="sign_up"),
     path('logout/', LogoutView.as_view(), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -5,19 +5,90 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Your email'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Your password'),
+            },
+            required=['username', 'password'],
+        ),
+        responses={
+            200: 'Successfully retrieved access and refresh tokens',
+            401: 'Invalid credentials',
+        },
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+class CustomTokenRefreshView(TokenRefreshView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'refresh': openapi.Schema(type=openapi.TYPE_STRING, description='Your refresh token'),
+            },
+            required=['refresh'],
+        ),
+        responses={
+            200: 'Successfully retrieved a new access token',
+            401: 'Invalid refresh token',
+        },
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('Authorization', openapi.IN_HEADER, description='Authorization header with Bearer token', type=openapi.TYPE_STRING, format=openapi.FORMAT_SLUG, default='Bearer <your_token_here>'),
+        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'refresh_token': openapi.Schema(type=openapi.TYPE_STRING, description='Your refresh token'),
+            },
+            required=['refresh_token'],
+        ),
+        responses={
+            200: 'Successfully logged out',
+            401: 'Invalid refresh token',
+        },
+    )
     def post(self, request):
         refresh_token = self.request.data.get('refresh_token')
         token = RefreshToken(token=refresh_token)
         token.blacklist()
         return Response({'detail': 'User logged out successfully.'}, status=status.HTTP_200_OK)
 
-
 class RegisterView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='Your username'),
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Your email'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Your password'),
+            },
+            required=['username', 'email', 'password'],
+        ),
+        responses={
+            201: 'User created successfully',
+            400: 'Bad Request: Invalid input data',
+        },
+    )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'detail': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'detail': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'detail': 'Bad Request: Invalid input data', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
